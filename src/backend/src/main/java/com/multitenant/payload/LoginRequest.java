262d4fa631bcec72cbf985daeb93d33f8633af1f
carinaa1 <sandra.sturza@stud.ubbcluj.ro>
2026-06-06 16:25:23 +0300

    task: add .gitignore, remove target\, fix backend startup

--- src/backend/src/main/java/com/multitenant/payload/LoginRequest.java
+++ src/backend/src/main/java/com/multitenant/payload/LoginRequest.java
@@ -2,8 +2,15 @@ package com.multitenant.payload;
 
 import lombok.Data;
 
-@Data
 public class LoginRequest {
     private String username;
     private String password;
+
+    public LoginRequest() {}
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
